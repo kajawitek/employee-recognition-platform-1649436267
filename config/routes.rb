884578b2cc2157211ed2202e93620fd709cd4b08
Kaja Witek <kajwitek@gmail.com>
2022-04-19 17:12:15 +0200

    added devise employee model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :employees
+  # get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+    Employee.create(name: "Employee #{i}", description: "An employee.", password: "Password", email: "email#{i}@test.com")
+  end
+  
