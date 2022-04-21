746222095f13e94fcf76bbbc0c37284e7916d0e5
kajawitek <kajwitek@gmail.com>
2022-04-21 13:46:28 +0200

    scaffold fixes

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -1,5 +1,6 @@
 class KudosController < ApplicationController
   before_action :set_kudo, only: %i[show edit update destroy]
+  before_action :authenticate_employee!
 
   # GET /kudos
   def index
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,7 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
+  has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
 end
