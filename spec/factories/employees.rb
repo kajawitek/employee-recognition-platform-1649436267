acf48093132cbb7738821d0ddbb40ef50b99b0b1
kajawitek <kajwitek@gmail.com>
2022-05-03 09:53:04 +0200

    code review  fixes

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :employee do
     sequence(:email) { |n| "test#{n}@test.com" }
     password { 'test123' }
+    number_of_available_kudos { 10 }
   end
 end
--- spec/features/spending_all_kudos_spec.rb
+++ spec/features/spending_all_kudos_spec.rb
@@ -1,26 +1,27 @@
 require 'rails_helper'
 # frozen_string_literal: true
 RSpec.describe 'Kudo spec', type: :feature do
-  let!(:kudo) { build(:kudo) }
+
+  let!(:kudo) { create(:kudo) }
 
   it 'spending all kudos' do
     visit root_path
 
+    
     fill_in 'employee[email]', with: kudo.giver.email
     fill_in 'employee[password]', with: kudo.giver.password
     click_button 'Log in'
 
     expect(page).to have_content 'success'
 
-    i = 10
-    while i > 0
+    while kudo.giver.number_of_available_kudos > 0
       click_link 'New Kudo'
       fill_in 'kudo[title]', with: kudo.title
       fill_in 'kudo[content]', with: kudo.content
       select kudo.receiver.email, from: 'kudo[receiver_id]'
       click_button 'Create Kudo'
       expect(page).to have_content 'success'
-      i -= 1
+      kudo.giver.reload
     end
 
     click_link 'New Kudo'
