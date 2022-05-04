4f80fbd3fb5181e4d1691703c30a3fb862c9e88f
kajawitek <kajwitek@gmail.com>
2022-05-04 19:35:03 +0200

    code review fixes

--- app/controllers/admins/employees_controller.rb
+++ app/controllers/admins/employees_controller.rb
@@ -1,6 +1,5 @@
 module Admins
   class EmployeesController < AdminsController
-    # GET /admins/kudos
     def index
       @employees = Employee.all
     end
@@ -21,15 +20,21 @@ module Admins
 
     def update
       @employee = Employee.find(params[:id])
-      if params[:employee][:password].blank?
-        @employee.update(employee_params_without_password)
 
+      if params[:employee][:password].length.between?(1, 6)
+        redirect_to admins_employees_url(@employee), notice: 'Your password is to short'
       else
-        @employee.update(employee_params)
 
+        if params[:employee][:password].blank?
+          @employee.update(employee_params_without_password)
+
+        else
+          @employee.update(employee_params)
+
+        end
+        @employee.save
+        redirect_to admins_employees_url(@employee), notice: 'Employee was successfully updated.'
       end
-      @employee.save
-      redirect_to admins_employees_url(@employee), notice: 'Employee was successfully updated.'
     end
 
     private
@@ -42,24 +47,4 @@ module Admins
       params.require(:employee).permit(:email, :number_of_available_kudos)
     end
   end
-
-  #       # DELETE /admins/kudos/1
-  #       def destroy
-  #         @kudo.destroy
-  #         redirect_to admins_kudos_url, notice: 'Kudo was successfully destroyed.'
-  #       end
-
-  #       private
-
-  #       # Use callbacks to share common setup or constraints between actions.
-  #       def set_kudo
-  #         @kudo = Kudo.find(params[:id])
-  #       end
-
-  #       # Only allow a list of trusted parameters through.
-  #       def kudo_params
-  #         params.require(:kudo).permit(:title, :content, :giver_id, :receiver_id)
-  #       end
 end
-
-#   # DELETE /kudos/1
