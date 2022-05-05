3103d25012d30bd0efe33d65fa899d59705a2ebf
kajawitek <kajwitek@gmail.com>
2022-05-05 20:50:10 +0200

    rubocop fixes

--- spec/models/company_value_spec.rb
+++ spec/models/company_value_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CompanyValue, type: :model do
-  let!(:company_value) { create(:company_value)}
+  let!(:company_value) { create(:company_value) }
 
   it 'title presence' do
     company_value.title = nil
     expect(company_value).not_to be_valid
   end
 
-  context 'title uniqueness validations' do
+  context 'when title is not unique' do
     before do
       same_title = company_value.dup
       same_title.save
     end
 
-    it { should_not be_valid }
+    it { is_expected.not_to be_valid }
   end
 end
