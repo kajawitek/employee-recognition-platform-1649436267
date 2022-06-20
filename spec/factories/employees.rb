39632255112964e73d996f27cd75e47ff9415447
kajawitek <kajwitek@gmail.com>
2022-06-20 18:35:26 +0200

    feat: cover new features with specs

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:email) { |n| "test#{n}@test.com" }
     password { 'test123' }
     number_of_available_kudos { 10 }
+    number_of_available_points { 0 }
   end
 end
