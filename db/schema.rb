3bf8920f04c4d0c0bb5558d4b84e1d09a8abf8e1
kajawitek <kajwitek@gmail.com>
2022-06-20 18:32:39 +0200

    feat: add number of available point column to emploees table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_150003) do
+ActiveRecord::Schema.define(version: 2022_06_20_154026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_06_07_150003) do
     t.string "name"
     t.text "description"
     t.integer "number_of_available_kudos", default: 10, null: false
+    t.integer "number_of_available_points", default: 0, null: false
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
