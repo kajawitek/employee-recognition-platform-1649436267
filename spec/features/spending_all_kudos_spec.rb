317911bc21bd7b320bfbc8071222eff191d91c06
kajawitek <kajwitek@gmail.com>
2022-06-20 18:36:41 +0200

    refactor: style fix in spending kudos spec

--- spec/features/spending_all_kudos_spec.rb
+++ spec/features/spending_all_kudos_spec.rb
@@ -15,8 +15,9 @@ RSpec.describe 'Kudo spec', type: :feature do
       fill_in 'kudo[title]', with: kudo.title
       fill_in 'kudo[content]', with: kudo.content
       select kudo.receiver.email, from: 'kudo[receiver_id]'
+      select kudo.company_value.title, from: 'kudo[company_value_id]'
       click_button 'Create Kudo'
-      expect(page).to have_content 'success'
+      expect(page).to have_content 'Kudo was successfully created.'
       kudo.giver.reload
     end
 
