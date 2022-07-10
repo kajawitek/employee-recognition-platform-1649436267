80b1ab0fef867365e4d70177ee30ce5ac75f77dc
kajawitek <kajawasiolek@gmail.com>
2022-07-10 10:23:36 +0200

    feat: add order model and declare relationships

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,5 @@ class Employee < ApplicationRecord
 
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
+  has_many :orders, dependent: :destroy
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,6 @@
 class Reward < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 1 }
   validates :title, :description, :price, presence: true
+
+  has_many :orders, dependent: :destroy
 end
