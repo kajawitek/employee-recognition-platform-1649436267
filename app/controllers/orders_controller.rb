8c97764a7b2f9d4485ce3f2e1ef5024d5177be45
kajawitek <kajawasiolek@gmail.com>
2022-07-10 10:26:42 +0200

    feat: add buying rewards with points feature for employee

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_employee!
+
+  def new
+    @order = Order.new
+    @reward = Reward.find(params[:reward])
+  end
+
+  def create
+    @order = Order.new
+    @reward = Reward.find(params[:reward])
+    if current_employee.number_of_available_points < @reward.price
+      redirect_to rewards_path, notice: 'Order was not created. Your number of available points is less than price'
+    else
+      @order.employee = current_employee
+      @order.reward = @reward
+      @order.employee.number_of_available_points -= @reward.price
+      begin
+        ActiveRecord::Base.transaction do
+          current_employee.save!
+          @order.save!
+        end
+        redirect_to rewards_url, notice: 'Order was successfully created.'
+      rescue ActiveRecord::RecordInvalid => e
+        render :new, notice: e.message, reward: @reward
+      end
+    end
+  end
+end
