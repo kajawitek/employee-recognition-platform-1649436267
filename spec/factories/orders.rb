3fb6d751fd85b6b0b18ce592af3896de3ce072ca
kajawitek <kajawasiolek@gmail.com>
2022-07-13 20:06:35 +0200

    feat: cover bought rewards view with specs

---
+++ spec/factories/orders.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :order do
+    reward
+    employee
+  end
+end
--- spec/features/order_spec.rb
+++ spec/features/order_spec.rb
@@ -3,15 +3,37 @@ require 'rails_helper'
 RSpec.describe 'Order spec', type: :feature do
   let!(:reward) { create(:reward, price: 10) }
   let!(:employee) { create(:employee, number_of_available_points: 11) }
+  let!(:order) { build(:order) }
+  let!(:admin) { create(:admin) }
 
   before do
     login_as employee, scope: :employee
     visit rewards_path
   end
 
-  it 'shows a lower number of points after buying a reward' do
+  it 'tests orders' do
+    # showing a lower number of points after buying a reward
     click_link 'Buy'
     expect(page).to have_content 'Order was successfully created'
     expect(page).to have_content 'Your points: 1'
+
+    # seeing bought rewards list by employees
+    visit orders_path
+    expect(page).to have_content reward.title
+    expect(page).to have_content reward.description
+    expect(page).to have_content order.purchase_price
+    expect(page).to have_content order.created_at
+
+    # checking if changing reward price does not affect the price in list of bought rewards
+    using_session('receiver session') do
+      login_as admin, scope: :admin
+      visit admins_rewards_path
+      click_link 'Edit'
+      fill_in 'reward[price]', with: '8'
+      click_button 'Update Reward'
+      expect(page).to have_content 'Reward was successfully updated.'
+    end
+    visit orders_path
+    expect(page).to have_content '10'
   end
 end
