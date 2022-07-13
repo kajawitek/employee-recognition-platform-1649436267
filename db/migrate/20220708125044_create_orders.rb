a2c14f24e4c6e3ee083eefb14cf816d9307c8bac
kajawitek <kajawasiolek@gmail.com>
2022-07-13 20:02:41 +0200

    feat: add purchase_price to orders table

--- db/migrate/20220708125044_create_orders.rb
+++ db/migrate/20220708125044_create_orders.rb
@@ -3,7 +3,8 @@ class CreateOrders < ActiveRecord::Migration[6.1]
     create_table :orders do |t|
       t.references :employee, null: false, foreign_key: true
       t.references :reward, null: false, foreign_key: true
-
+      t.decimal :purchase_price, null: false
+      
       t.timestamps
     end
   end
