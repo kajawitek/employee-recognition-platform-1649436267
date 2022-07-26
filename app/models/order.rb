a9e1514da1965726470af7aa041217233b58e9d6
kajawitek <kajawasiolek@gmail.com>
2022-07-26 17:35:51 +0200

    feat: add delivery status to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :employee
   belongs_to :reward
+
+  enum delivery_status: { ordered: 'ordered', delivered: 'delivered' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_125044) do
+ActiveRecord::Schema.define(version: 2022_07_26_112206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2022_07_08_125044) do
     t.decimal "purchase_price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "delivery_status", default: "ordered"
     t.index ["employee_id"], name: "index_orders_on_employee_id"
     t.index ["reward_id"], name: "index_orders_on_reward_id"
   end
