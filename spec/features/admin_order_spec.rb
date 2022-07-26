e1e0d253550cdd315d278e20b7a0fe1845a37bb4
kajawitek <kajawasiolek@gmail.com>
2022-07-26 17:40:18 +0200

    feat: cover marking orders as delivered with spec

--- spec/features/admin_order_spec.rb
+++ spec/features/admin_order_spec.rb
@@ -21,4 +21,21 @@ RSpec.describe 'Admin order spec', type: :feature do
     expect(page).to have_content time_ago_in_words(order.created_at)
     expect(page).to have_content order.purchase_price
   end
+  # There's a system spec for admin delivering an order
+
+  it 'tests admin delivering and order' do
+    click_link 'Orders'
+    expect(page).to have_content order.employee.email
+    expect(page).to have_content order.reward.title
+    expect(page).to have_content order.reward.description
+    expect(page).to have_content time_ago_in_words(order.created_at)
+    expect(page).to have_content order.purchase_price
+    expect(page).to have_content order.delivery_status
+
+    click_link 'Deliver'
+    expect(page).to have_content 'Order was successfully delivered.'
+
+    click_link 'Deliver'
+    expect(page).to have_content 'This order was delivered.'
+  end
 end
