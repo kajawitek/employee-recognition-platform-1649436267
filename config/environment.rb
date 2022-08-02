5979dcecf30562fa11c67b7673d0e94567733e2f
kajawitek <kajawasiolek@gmail.com>
2022-08-02 17:23:56 +0200

    fix: fix sendgrid configuration

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    # :password => Rails.application.credentials.api,
-    :password => Rails.application.credentials.dig(:sendgrid, :api_key),
-    :domain => 'https://emp-kw.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name: 'apikey',
+    password: Rails.application.credentials.dig(:sendgrid, :api_key),
+    domain: 'https://emp-kw.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
