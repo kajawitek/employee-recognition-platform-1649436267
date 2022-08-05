e7d29e3f60003001e6d53e4fabd18ae39faeaa28
kajawitek <kajawasiolek@gmail.com>
2022-08-05 19:26:49 +0200

    test: correct adding kudos to employees spec

--- spec/features/admin_adding_kudos_to_employees_spec.rb
+++ spec/features/admin_adding_kudos_to_employees_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Admin adding kudos to employee', type: :feature do
   let!(:admin) { create(:admin) }
   let!(:employee1) { create(:employee, number_of_available_kudos: 10) }
-  let!(:employee2) { create(:employee, number_of_available_kudos: 10) }
+  let!(:employee2) { create(:employee, number_of_available_kudos: 15) }
 
   let!(:company_value) { create(:company_value) }
 
@@ -17,24 +17,29 @@ RSpec.describe 'Admin adding kudos to employee', type: :feature do
     click_link 'Add Kudos'
     fill_in 'number_of_additional_kudos', with: '0'
     click_button 'Add kudos'
+
     expect(page).to have_content 'Additional kudos must be between 1 and 20'
-    expect(page).to have_content employee1.number_of_available_kudos = 10
-    expect(page).to have_content employee2.number_of_available_kudos = 10
 
     # checking adding 22 kudos
     click_link 'Add Kudos'
     fill_in 'number_of_additional_kudos', with: '22'
     click_button 'Add kudos'
     expect(page).to have_content 'Additional kudos must be between 1 and 20'
-    expect(page).to have_content employee1.number_of_available_kudos = 10
-    expect(page).to have_content employee2.number_of_available_kudos = 10
 
     # checking adding 10 kudos
+    employee1_kudos_number_before = employee1.reload.number_of_available_kudos
+    employee2_kudos_number_before = employee2.reload.number_of_available_kudos
+    added_kudos = rand(1..20)
+
     click_link 'Add Kudos'
-    fill_in 'number_of_additional_kudos', with: '10'
+    fill_in 'number_of_additional_kudos', with: added_kudos
     click_button 'Add kudos'
+
+    tr_employee1 = page.find("tr[employee-id=\"#{employee1.id}\"]")
+    tr_employee2 = page.find("tr[employee-id=\"#{employee2.id}\"]")
+
     expect(page).to have_content 'Additional kudos added'
-    expect(page).to have_content employee1.number_of_available_kudos = 20
-    expect(page).to have_content employee2.number_of_available_kudos = 20
+    expect(tr_employee1).to have_content employee1_kudos_number_before + added_kudos
+    expect(tr_employee2).to have_content employee2_kudos_number_before + added_kudos
   end
 end
