b983e9d22c44a07024923f79c794257976c3deee
kajawitek <kajawasiolek@gmail.com>
2022-08-08 19:58:20 +0200

    feat: add time limit and check employee policy for kudo and cover it with tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :employee_not_authorized
+
+  def pundit_user
+    current_employee
+  end
+
+  private
+
+  def employee_not_authorized
+    redirect_to root_path, notice: "You don't have access to this kudo"
+  end
 end
---
+++ spec/policies/kudo_policy_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe KudoPolicy do
+  subject(:kudo_policy) { described_class }
+
+  let(:employee1) { create(:employee) }
+  let(:employee2) { create(:employee) }
+  let!(:kudo) { create(:kudo, giver: employee1) }
+
+  permissions :edit?, :update?, :destroy? do
+    it 'grants access if employee is a giver' do
+      expect(kudo_policy).to permit(employee1, kudo)
+    end
+
+    it 'denies access if employee is not a giver' do
+      expect(kudo_policy).not_to permit(employee2, kudo)
+    end
+
+    it 'denies access if kudo created more than 5 minutes ago' do
+      travel 6.minutes do
+        expect(kudo_policy).not_to permit(employee1, kudo)
+      end
+    end
+  end
+end
