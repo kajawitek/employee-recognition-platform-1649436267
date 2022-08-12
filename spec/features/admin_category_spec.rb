e6b7cc3f4891157b3696b7cd5d7f63548be12396
kajawitek <kajawasiolek@gmail.com>
2022-08-12 15:53:46 +0200

    feat: cover creating, adding to reward and removing form reward category with specs

---
+++ spec/features/admin_category_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+RSpec.describe 'Admin category spec', type: :feature do
+  let!(:admin) { create(:admin) }
+  let!(:category) { build(:category) }
+  let!(:reward) { create(:reward) }
+
+  before do
+    login_as admin, scope: :admin
+    visit admins_root_path
+  end
+
+  it 'tests the creation of a category, adding it to the reward and removing it by admin' do
+    # creation of a category by admin
+    click_link 'Categories'
+    click_link 'New Category'
+    click_button 'Create Category'
+    expect(page).to have_content "Title can't be blank"
+    fill_in 'category[title]', with: category.title
+    click_button 'Create Category'
+    expect(page).to have_content 'Category was successfully created.'
+
+    # adding category to the reward by admin
+    click_link 'Rewards'
+    expect(page).to have_content 'no category'
+    click_link 'Edit'
+    select category.title, from: 'reward[category_id]'
+    click_button 'Update Reward'
+    expect(page).to have_content 'Reward was successfully updated.'
+    expect(page).to have_content category.title
+
+    # removing category from the reward by admin
+    click_link 'Edit'
+    click_link 'Remove category'
+    expect(page).to have_content 'Category removed.'
+    expect(page).to have_content 'no category'
+  end
+end
