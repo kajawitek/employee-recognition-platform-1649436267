c8a18bbb4a704b58e6351d37e7249d8ec6c1d185
kajawitek <kajawasiolek@gmail.com>
2022-08-17 12:28:17 +0200

    feat: add rewards filter option for employee

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -2,7 +2,8 @@ class RewardsController < ApplicationController
   before_action :authenticate_employee!
 
   def index
-    @rewards = Reward.all.page params[:page]
+    @categories = Category.all
+    @rewards = RewardsQuery.new(categories: @categories, rewards: (Reward.all.page params[:page]), params: params).call.includes([:category])
   end
 
   def show
