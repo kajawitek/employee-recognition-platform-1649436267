d86dc8f215c3fcd79ec73d970f63c2451c8c3000
kajawitek <kajawasiolek@gmail.com>
2022-08-20 12:38:05 +0200

    feat: add reward photo upload/show

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,9 +1,11 @@
 class Reward < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 1 }
   validates :title, :description, :price, presence: true
+  validates :reward_photo, blob: { content_type: ['image/png', 'image/jpg'] }
 
   has_many :orders, dependent: :destroy
   belongs_to :category, optional: true
+  has_one_attached :reward_photo
 
   paginates_per 3
 end
