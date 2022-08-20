3043dc530077795a7e8096d7423453d01f1e67ce
kajawitek <kajawasiolek@gmail.com>
2022-08-20 12:36:29 +0200

    build: config app

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec rails s
+release: bundle exec rake db:migrate
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -111,7 +111,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'emp-kw.herokuapp.com' }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
