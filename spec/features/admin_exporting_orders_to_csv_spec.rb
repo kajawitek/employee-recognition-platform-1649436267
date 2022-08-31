4893e8684cf6a6f2f18382564f891c5a30b80d36
kajawitek <kajawasiolek@gmail.com>
2022-08-31 19:45:52 +0200

    feat: cover orders export option by admin with spec

---
+++ spec/features/admin_exporting_orders_to_csv_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+RSpec.describe 'Admin exporting order spec', type: :feature do
+  let!(:admin) { create(:admin) }
+  let!(:order1) { create(:order) }
+  let!(:order2) { create(:order) }
+
+  before do
+    login_as admin, scope: :admin
+    visit admins_root_path
+  end
+
+  it 'tests admin exporting orders to csv' do
+    click_link 'Orders'
+    click_link 'Export'
+
+    csv = CSV.parse(page.body)
+
+    expect(csv).to have_content order1.employee.id
+    expect(csv).to have_content order2.employee.id
+    expect(csv).to have_content order1.reward.id
+    expect(csv).to have_content order2.reward.id
+    expect(csv).to have_content order1.purchase_price
+    expect(csv).to have_content order2.purchase_price
+    expect(csv).to have_content order1.created_at
+    expect(csv).to have_content order2.created_at
+    expect(csv).to have_content order1.updated_at
+    expect(csv).to have_content order2.updated_at
+    expect(csv).to have_content order1.delivery_status
+    expect(csv).to have_content order2.delivery_status
+  end
+end
