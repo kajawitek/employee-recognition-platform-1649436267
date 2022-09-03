7d4c91c383557ac729f710b87b94130ff65feab8
kajawitek <kajawasiolek@gmail.com>
2022-09-03 18:54:41 +0200

    refactor: add csv template to admin orders export feature

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,6 @@
-require 'csv'
 class Order < ApplicationRecord
   belongs_to :employee
   belongs_to :reward
 
   enum delivery_status: { ordered: 'ordered', delivered: 'delivered' }
-
-  def self.to_csv
-    orders = all
-    CSV.generate do |csv|
-      csv << column_names
-      orders.each do |order|
-        csv << order.attributes.values_at(*column_names)
-      end
-    end
-  end
 end
