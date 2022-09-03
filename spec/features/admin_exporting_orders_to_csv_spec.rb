f5cf3422b3ea92a7779ebceae5ea948cb05df996
kajawitek <kajawasiolek@gmail.com>
2022-09-03 18:56:30 +0200

    test: update spec after adding template for exporting orders by the admin

--- spec/features/admin_exporting_orders_to_csv_spec.rb
+++ spec/features/admin_exporting_orders_to_csv_spec.rb
@@ -15,17 +15,25 @@ RSpec.describe 'Admin exporting order spec', type: :feature do
 
     csv = CSV.parse(page.body)
 
-    expect(csv).to have_content order1.employee.id
-    expect(csv).to have_content order2.employee.id
+    expect(csv).to have_content order1.delivery_status
+    expect(csv).to have_content order2.delivery_status
+    expect(csv).to have_content order1.id
+    expect(csv).to have_content order2.id
     expect(csv).to have_content order1.reward.id
     expect(csv).to have_content order2.reward.id
-    expect(csv).to have_content order1.purchase_price
-    expect(csv).to have_content order2.purchase_price
+    expect(csv).to have_content order1.reward.title
+    expect(csv).to have_content order2.reward.title
+    expect(csv).to have_content order1.reward.description
+    expect(csv).to have_content order2.reward.description
     expect(csv).to have_content order1.created_at
     expect(csv).to have_content order2.created_at
     expect(csv).to have_content order1.updated_at
     expect(csv).to have_content order2.updated_at
-    expect(csv).to have_content order1.delivery_status
-    expect(csv).to have_content order2.delivery_status
+    expect(csv).to have_content order1.purchase_price
+    expect(csv).to have_content order2.purchase_price
+    expect(csv).to have_content order1.employee.id
+    expect(csv).to have_content order2.employee.id
+    expect(csv).to have_content order1.employee.email
+    expect(csv).to have_content order2.employee.email
   end
 end
