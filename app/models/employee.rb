688f15c1b6ba38ba7ec5813f84afafacb8dd8dfa
kajawitek <kajawasiolek@gmail.com>
2022-09-19 12:57:58 +0200

    feat: add first_name and last_name to employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,10 @@ class Employee < ApplicationRecord
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
   has_many :orders, dependent: :destroy
+
+  validates :first_name, :last_name, presence: true
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
