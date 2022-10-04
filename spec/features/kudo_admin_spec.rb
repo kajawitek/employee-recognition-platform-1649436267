dbeaf4418298f9b34684f9f78d561ea6ce6bd944
kajawitek <kajawasiolek@gmail.com>
2022-10-04 14:59:27 +0200

    test: remove unnecessary if statements from kudo_admin_spec, admin_order_spec and kudo_spec

--- spec/features/kudo_admin_spec.rb
+++ spec/features/kudo_admin_spec.rb
@@ -14,18 +14,8 @@ RSpec.describe 'Kudo spec', type: :feature do
     click_link 'Kudos'
     expect(page).to have_content kudo.title
     expect(page).to have_content kudo.content
-
-    if kudo.giver.full_name.present?
-      expect(page).to have_content kudo.giver.full_name
-    else
-      expect(page).to have_content kudo.giver.email
-    end
-
-    if kudo.receiver.full_name.present?
-      expect(page).to have_content kudo.receiver.full_name
-    else
-      expect(page).to have_content kudo.receiver.email
-    end
+    expect(page).to have_content kudo.giver.full_name
+    expect(page).to have_content kudo.receiver.full_name
 
     # destroying kudos by admin
     click_link 'Destroy'
