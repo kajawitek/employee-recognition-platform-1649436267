704019a175d270f57f092ecf7e29eda8d59b6ed8
kajawitek <kajawasiolek@gmail.com>
2022-10-04 14:50:47 +0200

    test: add faker gem  and use it in factories/employees

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ group :development, :test do
   gem 'bullet'
   gem 'capybara', '~> 3.36'
   gem 'factory_bot_rails'
+  gem 'faker'
   gem 'rspec-rails', '~> 5.0.0'
   gem 'shoulda-matchers', '~> 5.0'
 end
--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :employee do
-    sequence(:first_name) { |n| "First name #{n}" }
-    sequence(:last_name) { |n| "Last name #{n}" }
-    sequence(:email) { |n| "test#{n}@test.com" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    sequence(:email) { "#{first_name}.#{last_name}@test.com" }
     password { 'test123' }
     number_of_available_kudos { 10 }
     number_of_available_points { 0 }
