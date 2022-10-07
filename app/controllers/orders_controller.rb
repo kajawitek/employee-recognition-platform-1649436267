304be8d2a3aece7e975b43f89315a239f787e0e7
kajawitek <kajawasiolek@gmail.com>
2022-10-07 13:22:21 +0200

    feat: add delivery_method to buying rewards, add address fields for post delivery method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,18 @@ class OrdersController < ApplicationController
     @orders = OrdersQuery.new(employee: current_employee, params: params).call
   end
 
+  def new
+    @order = Order.new(reward: Reward.find(params[:reward]))
+    @address = Address.find_or_initialize_by(employee: current_employee)
+    return unless current_employee.number_of_available_points < @order.reward.price
+
+    redirect_to rewards_path, notice: 'Your number of available points is less than price'
+  end
+
   def create
     @order = Order.new
-    @reward = Reward.find(params[:reward])
+    @reward = Reward.find(order_params[:reward])
+    @address = Address.find_or_initialize_by(employee: current_employee)
     if current_employee.number_of_available_points < @reward.price
       redirect_to rewards_path, notice: 'Order was not created. Your number of available points is less than price'
     else
@@ -19,11 +28,22 @@ class OrdersController < ApplicationController
         ActiveRecord::Base.transaction do
           current_employee.save!
           @order.save!
+          @address.update!(address_params) if @reward.post?
         end
         redirect_to orders_url, notice: 'Order was successfully created.'
       rescue ActiveRecord::RecordInvalid => e
         render :new, notice: e.message, reward: @reward
       end
     end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:reward)
+  end
+
+  def address_params
+    params.require(:address).permit(:street, :postcode, :city)
   end
 end
