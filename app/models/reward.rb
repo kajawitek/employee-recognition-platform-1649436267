3c0575e82f1980ab927e5e6efed39cc7a5934f38
Kaja Witek <kaja.wasiolek@gmail.com>
2022-12-08 23:14:16 +0100

    feat: add number_of_available_items to rewards

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,12 +3,17 @@ class Reward < ApplicationRecord
   validates :title, :description, :price, presence: true
   validates :title, uniqueness: true
   validates :reward_photo, blob: { content_type: ['image/png', 'image/jpg'] }
+  validates :number_of_available_items, presence: true, if: :post?
+  validates :number_of_available_items, numericality: { greater_than_or_equal_to: 0 }, if: :online?
 
   has_many :orders, dependent: :destroy
+  has_many :online_codes, dependent: :destroy
   belongs_to :category, optional: true
   has_one_attached :reward_photo
 
   paginates_per 3
 
   enum delivery_method: { online: 'online', post: 'post' }
+
+  scope :available, -> { where('number_of_available_items > 0') }
 end
