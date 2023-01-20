0e6c36fcfa2f3a94776065be8e21a12d35d34332
Kaja Witek <kaja.wasiolek@gmail.com>
2023-01-20 14:15:55 +0100

    test: update admin_order_spec with pick-up delivery case

--- spec/features/admin_order_spec.rb
+++ spec/features/admin_order_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 # frozen_string_literal: true
 RSpec.describe 'Admin order spec', type: :feature do
   let!(:admin) { create(:admin) }
-  let!(:order) { create(:order, reward: create(:reward, :post)) }
+  let!(:employee) { create(:employee) }
+  let!(:order_post) { create(:order, reward: create(:reward, :post), employee: employee) }
+  let!(:order_pick_up) { create(:order, reward: create(:reward, :pick_up), employee: employee) }
 
   before do
     login_as admin, scope: :admin
@@ -11,30 +13,48 @@ RSpec.describe 'Admin order spec', type: :feature do
 
   it "tests if admin sees employee's bought rewards list" do
     click_link 'Employees'
-    expect(page).to have_content order.employee.email
-    expect(page).to have_content order.employee.number_of_available_kudos
+    expect(page).to have_content order_post.employee.email
+    expect(page).to have_content order_post.employee.number_of_available_kudos
 
     click_link 'Show'
-    expect(page).to have_content order.employee.email
-    expect(page).to have_content order.reward.title
-    expect(page).to have_content order.reward.description
-    expect(page).to have_content time_ago_in_words(order.created_at)
-    expect(page).to have_content order.purchase_price
+    expect(page).to have_content order_post.employee.email
+    expect(page).to have_content order_post.reward.title
+    expect(page).to have_content order_post.reward.description
+    expect(page).to have_content time_ago_in_words(order_post.created_at)
+    expect(page).to have_content order_post.purchase_price
   end
 
   it 'tests admin delivering and order' do
     click_link 'Orders'
 
-    expect(page).to have_content order.employee.full_name
-    expect(page).to have_content order.reward.title
-    expect(page).to have_content order.reward.description
-    expect(page).to have_content time_ago_in_words(order.created_at)
-    expect(page).to have_content order.purchase_price
-    expect(page).to have_content order.delivery_status
+    expect(page).to have_content order_post.employee.full_name
+    expect(page).to have_content order_post.reward.title
+    expect(page).to have_content order_post.reward.description
+    expect(page).to have_content time_ago_in_words(order_post.created_at)
+    expect(page).to have_content order_post.purchase_price
+    expect(page).to have_content order_post.delivery_status
 
     click_link 'Deliver'
     expect(page).to have_content 'Order was successfully delivered.'
+    expect(page).not_to have_content('Deliver, link')
+  end
 
+  it 'tests admin preparing and delivering pick-up rewards' do
+    click_link 'Orders'
+
+    expect(page).to have_content order_pick_up.employee.full_name
+    expect(page).to have_content order_pick_up.reward.title
+    expect(page).to have_content order_pick_up.reward.description
+    expect(page).to have_content time_ago_in_words(order_pick_up.created_at)
+    expect(page).to have_content order_pick_up.purchase_price
+    expect(page).to have_content order_pick_up.delivery_status
+
+    click_link 'Ready for pick-up'
+    expect(page).to have_content 'Pick-up instruction sent!'
+    expect(page).not_to have_content('Ready for pick-up, link')
+
+    find(:xpath, "//tr[td[contains(.,'ready for pick-up')]]/td/a", text: 'Deliver').click
+    expect(page).to have_content 'Order was successfully delivered.'
     expect(page).not_to have_content('Deliver, link')
   end
 end
